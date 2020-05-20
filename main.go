bd45617d65c1f4ef1a77017c5e818e9949783bbf
Yhojo <hojo.yosuke@gmail.com>
2020-05-20 22:22:42 +0900

    Add additional routing logic and dummy data for it

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"pkg/parser"
@@ -8,10 +9,24 @@ import (
 
 func main() {
 	parser.Read("./config/root.json")
-	http.HandleFunc("/", HelloHandler)
-	// http.ListenAndServe(":8888", nil)
+	// http.HandleFunc("/", HelloHandler)
+	for i := 0; i < 5; i++ {
+		addHandler(i)
+	}
+	http.ListenAndServe(":8888", nil)
 }
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World!\n"))
+	w.Write([]byte("Hello, World! HOME\n"))
+}
+
+func createFunc(i int) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(fmt.Sprintf("Hello, World! No.%d\n", i)))
+	}
+}
+
+func addHandler(i int) {
+	rooting := fmt.Sprintf("/dummy%d", i)
+	http.HandleFunc(rooting, createFunc(i))
 }
