940e7719018c4dfc4c3c65bde8133e2913f6aa54
Yhojo <hojo.yosuke@gmail.com>
2020-05-24 17:39:16 +0900

    Tests reflect the impact of Parser changes

--- src/pkg/parser/json_test.go
+++ src/pkg/parser/json_test.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -10,7 +11,36 @@ import (
 )
 
 func TestJsonReader(t *testing.T) {
-	testJSONStr := []byte(`[{"Root": "testRoot1", "Response": "I'm Response"}]`)
+	// testJSONStr := []byte(`[{"Root": "testRoot1", "Response":[{ "Text": "I'm Response", "Status": 200}]]`)
+	testJSONStr := []byte(`
+[
+  {
+    "Root": "testRoot1",
+    "Response": [
+      {
+        "method": "GET",
+        "text": "I'm Response",
+        "status": 500
+      },
+      {
+        "method": "POST",
+        "text": "Receive POST req",
+        "status": 301
+      }
+    ],
+    "Children": [
+      {
+        "Root": "TestRootChirdren1",
+        "Response": [
+          {
+            "text": "Children",
+            "status": 404
+          }
+        ]
+      }
+    ]
+  }
+]`)
 	tmpf, _ := ioutil.TempFile("", "exam")
 	defer os.Remove(tmpf.Name())
 
@@ -19,6 +49,11 @@ func TestJsonReader(t *testing.T) {
 	}
 
 	res := Read(tmpf.Name())
+	fmt.Printf("%+v\n", res)
 	assert.Equal(t, (*res)[0].Root, "testRoot1")
-	assert.Equal(t, (*res)[0].Response, "I'm Response")
+	assert.Equal(t, (*res)[0].Response[0].Text, "I'm Response")
+	assert.Equal(t, (*res)[0].Response[0].Status, 500)
+	assert.Equal(t, (*res)[0].Response[1].Text, "Receive POST req")
+	assert.Equal(t, (*res)[0].Response[1].Status, 301)
+	assert.Equal(t, (*res)[0].Children[0].Root, "TestRootChirdren1")
 }
